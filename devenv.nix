39d46adc6ee8720bc33334c086f50beba921ffad
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2026-08-19 19:37:31 +0200

    build: update dev environment
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -3,12 +3,15 @@
 {
   dotenv.enable = true;
 
+  dagger.enable = true;
+  env.DAGGER_X_RELEASE = "v1.0.0-beta.9";
+
   packages = with pkgs; [
     lld
 
     cargo-audit
     cargo-deny
-    cargo-expand
+    cargo-dist
     cargo-fuzz
     cargo-release
     cargo-watch
